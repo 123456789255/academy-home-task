7ef631cc80404115261d0fcc1801b3ceed1dcfd0
home <kazah.zanat@gmail.com>
2023-03-09 06:04:37 +0600

    Done
    all of the work is done!!!!

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -5,7 +5,7 @@
         <div class="d-flex justify-content-center">
             <a href="{{ route('admin.orders') }}" class="btn btn-success m-auto">Просмотр и редактирование заказов</a>
             <a href="{{ route('admin.products') }}" class="btn btn-primary m-auto">Добавление и редактирование товаров в каталоге</a>
-            <a href="" class="btn btn-danger m-auto">Добавление и удаление категорий</a>
+            <a href="{{ route('admin.category') }}" class="btn btn-danger m-auto">Добавление и удаление категорий</a>
         </div>
     </div>
 @endsection
